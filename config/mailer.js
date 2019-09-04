865fdfaebcc60004e8a1699c82470005f469641c
tanujvyas10 <48439116+tanujvyas10@users.noreply.github.com>
2019-09-05 01:40:59 +0530

    Update mailer.js

--- config/mailer.js
+++ config/mailer.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MAILGUN_USER: 'postmaster@sandbox9d8badd3fc1e413fb4765208d49949b9.mailgun.org',
-    MAILGUN_PASS: 'a63665f61676c481e2296ca8b09a5e2f-acb0b40c-953f950e'
+    MAILGUN_USER: '',
+    MAILGUN_PASS: ''
   };
   
